bdcd507ef6727bb4bc4b0aa9d8a9b30309c1fe96
HarmanjotMalhi <harman@harmanjots-air.home>
2025-02-14 17:51:22 -0500

    Changed to HashedRouter

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
